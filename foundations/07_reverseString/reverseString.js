7ab374ab97798406288a6c672d5529fd6cacb7d6
LixdyDev <anurarohana1239@gmail.com>
2026-01-12 04:39:09 +0530

    feat(foundations): complete javascript exercises 06-11
    Completed the following exercises:
    - 06_repeatString: implemented string repetition using loops
    - 07_reverseString: implemented string reversal using split/reverse/join
    - 08_removeFromArray: implemented filtering using rest parameters
    - 09_sumAll: implemented range summation with input validation
    - 10_leapYears: implemented leap year logic handling century rules
    - 11_tempConversion: implemented unit conversion with specific rounding

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    
+    let char = '';
+    for(let i = 0; i < str.length; i++ ) {
+        char += str[(str.length - 1) - i];
+    }   
+    return char;
 };
 
 // Do not edit below this line
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(min,max) {
 
+    if(!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if(min < 0 || max < 0) return "ERROR";
+
+    let minNum = Math.min(min, max);
+    let maxNum = Math.max(min, max);
+    
+    let sum = 0; 
+    for(let i = minNum; i <= maxNum; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
